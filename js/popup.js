70739e7dd4dc3d6bba02cded50b4ff21aa8adad4
Julia McClellan <juliamcclellan37@gmail.com>
2018-01-28 10:33:06 -0500

    Add blocked pages and domains

--- js/popup.js
+++ js/popup.js
@@ -2,4 +2,16 @@ $(document).ready(function() {
   document.getElementById("sites").addEventListener("click", function(){
     window.location.href = "blockedSites.html";
   });
+
+document.getElementById("addP").addEventListener("click", function(){
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+      addBlockedPage(tabs[0].url)
+    });
+  });
+
+  document.getElementById("addD").addEventListener("click", function(){
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+      addBlockedDomain(tabs[0].url)
+    });
+  });
 });
