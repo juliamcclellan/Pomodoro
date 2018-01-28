96d88a870e7dd321160ddf2085ca6f83afe4fc36
Julia McClellan <juliamcclellan37@gmail.com>
2018-01-28 00:35:28 -0500

    Semi working blocking pages

--- js/block.js
+++ js/block.js
@@ -1,4 +1,3 @@
-
 chrome.tabs.onCreated.addListener(createdListener);
 chrome.tabs.onUpdated.addListener(updateListener);
 
@@ -11,15 +10,44 @@ function updateListener(tabId, changeInfo, tab) {
 }
 
 function block(url, id) {
-  if(url != undefined && getHostName(url) == "facebook.com") {//url.includes("facebook.com")) {
-    chrome.tabs.update(id, {url: "htmlcss/redirect.html"});
-  }
+  chrome.storage.local.get(["blockedPages", "blockedDomains"], function(blocked) {
+    console.log(blocked);
+    if(blocked != undefined && url != undefined) {
+      var bool = false;
+
+      var pages = blocked.blockedPages;
+      //console.log(pages);
+      for (i = 0; i < pages.length; i++) {
+        if(pages[i] == url) {
+          bool = true;
+          break;
+        }
+      }
+
+      if(!bool) {
+        url = getHostName(url);
+        var domains = blocked.blockedDomains;
+        //console.log(domains);
+        for (i = 0; i < domains.length; i++) {
+          if(domains[i] == url) {
+            bool = true;
+            break;
+          }
+        }
+      }
+
+      if(bool) {
+        chrome.tabs.update(id, {url: "htmlcss/redirect.html"});
+      }
+    }
+  });
 };
 
 // From primaryobjects.com
 function getHostName(url) {
     var match = url.match(/:\/\/(www[0-9]?\.)?(.[^/:]+)/i);
-    if (match != null && match.length > 2 && typeof match[2] === 'string' && match[2].length > 0) {
+    if (match != null && match.length > 2 && typeof match[2] === 'string'
+      && match[2].length > 0) {
     return match[2];
     }
     else {
--- js/storage.js
+++ js/storage.js
@@ -1,3 +1,17 @@
-function getBlocked() {
+chrome.storage.local.set({blockedDomains: [], blockedPages: []});
+
+function addBlockedPage(url) {
+  chrome.storage.local.get("blockedPages", function(blocked) {
+    if(blocked != undefined) {
+      blocked.blockedPages.push(url);
+      chrome.storage.local.set({"blockedPages": blocked.blockedPages});
+    } else {
+      chrome.storage.local.set({"blockedPages": [url]});
+    }
+  });
+}
+
+function addBlockedDomain(url) {
+  url = getHostName(url);
 
 }
