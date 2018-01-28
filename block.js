d5c2c5f364ff567985e5302158810ac09749e89e
Julia McClellan <juliamcclellan37@gmail.com>
2018-01-27 21:02:28 -0500

    Git is making me do this

---
+++ block.js
@@ -0,0 +1,30 @@
+
+chrome.tabs.onCreated.addListener(createdListener);
+chrome.tabs.onUpdated.addListener(updateListener);
+
+function createdListener(tab) {
+  block(tab.url, tab.id);
+}
+
+function updateListener(tabId, changeInfo, tab) {
+  block(changeInfo.url, tab.id);
+}
+
+function block(url, id) {
+  if(url != undefined && url.includes("facebook.com")) {
+    chrome.tabs.update(id, {url: "https://www.google.com"});
+  }
+};
+
+// From primaryobjects.com
+/*
+function getHostName(url) {
+    var match = url.match(/:\/\/(www[0-9]?\.)?(.[^/:]+)/i);
+    if (match != null && match.length > 2 && typeof match[2] === 'string' && match[2].length > 0) {
+    return match[2];
+    }
+    else {
+        return null;
+    }
+}
+*/
